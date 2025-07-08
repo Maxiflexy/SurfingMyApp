c71fbb82b547563100fdbfd8e90b39f9f98e19eb
maxiflexy <onyemax247@gmail.com>
2025-07-08 17:32:09 +0100

    init commit

---
+++ omnexa-merchant-backoffice/src/main/java/com/digicore/omnexa_merchant_backoffice/OmnexaMerchantBackofficeApplication.java
@@ -0,0 +1,13 @@
+package com.digicore.omnexa_merchant_backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnexaMerchantBackofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnexaMerchantBackofficeApplication.class, args);
+	}
+
+}
---
+++ omnexa-merchant-backoffice/src/test/java/com/digicore/omnexa_merchant_backoffice/OmnexaMerchantBackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digicore.omnexa_merchant_backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnexaMerchantBackofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
